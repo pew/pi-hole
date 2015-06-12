058f0d6f5d90629033be50a43f6bb1a0cce0e923
John Doe <john@example.com>
2015-06-11 17:27:57 -0700

    stop all this sorting

--- gravity-adv.sh
+++ gravity-adv.sh
@@ -16,28 +16,28 @@ else
 fi
 
 echo "Getting yoyo ad list..." # Approximately 2452 domains at the time of writing
-curl -s -d mimetype=plaintext -d hostformat=unixhosts http://pgl.yoyo.org/adservers/serverlist.php? | sort > /tmp/matter.txt
+curl -s -d mimetype=plaintext -d hostformat=unixhosts http://pgl.yoyo.org/adservers/serverlist.php? > /tmp/matter.txt
 
 echo "Getting winhelp2002 ad list..." # 12985 domains
-curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' | sort >> /tmp/matter.txt
+curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting adaway ad list..." # 445 domains
-curl -s https://adaway.org/hosts.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}'| sort >> /tmp/matter.txt
+curl -s https://adaway.org/hosts.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting hosts-file ad list..." # 28050 domains
-curl -s http://hosts-file.net/ad_servers.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' | sort >> /tmp/matter.txt
+curl -s http://hosts-file.net/ad_servers.txt | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting malwaredomainlist ad list..." # 1352 domains
-curl -s http://www.malwaredomainlist.com/hostslist/hosts.txt |egrep -v '^#'|sed '/^$/d'|awk '{print $2}'| sort >> /tmp/matter.txt
+curl -s http://www.malwaredomainlist.com/hostslist/hosts.txt |egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting adblock.gjtech ad list..." # 696 domains
-curl -s http://adblock.gjtech.net/?format=unix-hosts | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' | sort >> /tmp/matter.txt
+curl -s http://adblock.gjtech.net/?format=unix-hosts | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting someone who cares ad list..." # 10600
-curl -s http://someonewhocares.org/hosts/hosts | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' | sort >> /tmp/matter.txt
+curl -s http://someonewhocares.org/hosts/hosts | egrep -v '^#'|sed '/^$/d'|awk '{print $2}' >> /tmp/matter.txt
 
 echo "Getting Mother of All Ad Blocks list..." # 102168 domains!! Thanks Kacy
-curl -s -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ http://adblock.mahakala.is/ | egrep -v '^#' | awk '{print $2}' | sort >> /tmp/matter.txt
+curl -s -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ http://adblock.mahakala.is/ | egrep -v '^#' | awk '{print $2}' >> /tmp/matter.txt
 
 # Add entries from the local blacklist file if it exists in /etc/pihole directory
 if [[ -f $blacklist ]];then
@@ -51,12 +51,12 @@ fi
 # Remove entries from the whitelist file if it exists at the root of the current user's home folder
 if [[ -f $whitelist ]];then
 	echo "Removing duplicates, whitelisting, and formatting the list of domains..."
-	cat /tmp/matter.txt | sed $'s/\r$//' | sort | uniq | sed '/^$/d' | grep -v -x -f $whitelist | awk -v "IP=$piholeIP" '{sub(/\r$/,""); print "address=/"$0"/"IP}' > /tmp/andLight.txt
+	cat /tmp/matter.txt|sed $'s/\r$//' | uniq | sed '/^$/d' | grep -v -x -f $whitelist | awk -v "IP=$piholeIP" '{sub(/\r$/,""); print "address=/"$0"/"IP}' > /tmp/andLight.txt
 	numberOfSitesWhitelisted=$(cat $whitelist | wc -l | sed 's/^[ \t]*//')
 	echo "$numberOfSitesWhitelisted domains whitelisted."
 else
 	echo "Removing duplicates and formatting the list of domains..."
-	cat /tmp/matter.txt | sed $'s/\r$//' | sort | uniq | sed '/^$/d' | awk -v "IP=$piholeIP" '{sub(/\r$/,""); print "address=/"$0"/"IP}' > /tmp/andLight.txt
+	cat /tmp/matter.txt|sed $'s/\r$//'|uniq|sed '/^$/d'|awk -v "IP=$piholeIP" '{sub(/\r$/,""); print "address=/"$0"/"IP}' > /tmp/andLight.txt
 fi
 
 # Count how many domains/whitelists were added so it can be displayed to the user
